ba10ea76fd4216c6c6d010c98daeb683b2e5dc71
basantiscits <basantgera29@gmail.com>
2019-01-29 00:26:06 -0500

    Master_Page_Made_28_JAN

--- SOEN6441_Project/src/com/proj/App/App.java
+++ SOEN6441_Project/src/com/proj/App/App.java
@@ -1,13 +1,15 @@
 package com.proj.app;
-import com.proj.controller.Controller;
-import com.proj.model.Model;
-import com.proj.views.View;
+//import com.proj.controller.Controller;
+//import com.proj.model.Model;
+//import com.proj.views.View;
+import com.proj.views.*;
 
 public class App {
 	public static void main(String[] args) {
-		  Model m = new Model("Basant", "Gera",26);
-		  View v = new View("SOEN 6441");
-		  Controller c = new Controller(m, v);
-		  c.initController();
+		MainMenuScreen v= new MainMenuScreen();
+		 // Model m = new Model("Basant", "Gera",26);
+		 // View v = new View("SOEN 6441");
+		  //Controller c = new Controller(m, v);
+		  //c.initController();
 	}
 }
