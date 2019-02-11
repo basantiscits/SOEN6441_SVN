e1124e087b1e1ffc168597c3542ec5a53fb7dc13
arpitmalhotra <arpitmalhotra12@gmail.com>
2019-02-11 04:23:59 -0500

    Updated addCountries method to correctly set list of neighbours

--- SOEN6441_Project/src/com/proj/controllers/CreateMapContoller.java
+++ SOEN6441_Project/src/com/proj/controllers/CreateMapContoller.java
@@ -9,97 +9,89 @@ import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
+
 import com.proj.models.Continent;
 import com.proj.models.ContinentArea;
 import com.proj.models.Country;
 import com.proj.models.Map;
-import com.proj.views.CountriesArea;
+
 import com.proj.views.CreateMapEditor;
 
-
-
-
-
-
 public class CreateMapContoller implements ActionListener {
-	
+
 	private Continent continent;
-	
+
 	private CreateMapEditor createMapEditor;
-	
+
 	private Map map;
-	
+
 	private ContinentArea continentArea;
-	
-	private CountriesArea countriesArea;
-	
+
 	private ArrayList<Country> addCountry = new ArrayList<Country>();
-	
+
 	public CreateMapContoller(CreateMapEditor createMapEditor) {
 		this.createMapEditor = createMapEditor;
 	}
-	
+
 	public void addMap(Map map) {
-		this.map = map;	
+		this.map = map;
 	}
-	
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		String button = e.getActionCommand();
-		switch(button) {
-			case "Add Continent":
+		switch (button) {
+		case "Add Continent":
 			addContinent();
 			break;
-			case "Add Countries": 
+		case "Add Countries":
 			addCountries();
 			break;
-			default:
+		default:
 			break;
 		}
-		
-	}
-	
-	public void addContinent() {
-				boolean loop = true;
-				continent = new Continent();
-				while(loop) {
-					String continentName = JOptionPane.showInputDialog(null, "Enter the Continent name: ","Add Continent",JOptionPane.OK_CANCEL_OPTION|JOptionPane.QUESTION_MESSAGE);
-					if(continentName !=null) {
-						if(continentName.isEmpty()) {
-							JOptionPane.showMessageDialog(null, "Please specify the name!");
-						}
-						
-						
-						else if(continentName.trim().isEmpty()) {
-							JOptionPane.showMessageDialog(null, "Name cannot be empty!");
-						}
-						
-						else if(map.searchContinent(continentName).equalsIgnoreCase(continentName)) {
-							JOptionPane.showMessageDialog(null, "Continent name already exists!");
-						}
-						
-						else if(map.searchContinent(continentName.trim()).equalsIgnoreCase(continentName.trim())) {
-							JOptionPane.showMessageDialog(null, "Continent name already exists!");
-						}
-						
-						else if ((continentName != null) && (createMapEditor.getMap().searchContinent(continentName)=="")) {
-							continentArea = new ContinentArea(map);
-							continent.setContinentName(continentName);
-							createMapEditor.getMap().addContinent(continent);
-							createMapEditor.createTree();
-							loop = false;
-						}
-					}
-					else {
-						loop = false;
-					}
 
-				}
-				
 	}
-	
+
+	public void addContinent() {
+		boolean loop = true;
+		continent = new Continent();
+		while (loop) {
+			String continentName = JOptionPane.showInputDialog(null, "Enter the Continent name: ", "Add Continent",
+					JOptionPane.OK_CANCEL_OPTION | JOptionPane.QUESTION_MESSAGE);
+			if (continentName != null) {
+				if (continentName.isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Please specify the name!");
+				}
+
+				else if (continentName.trim().isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Name cannot be empty!");
+				}
+
+				else if (map.searchContinent(continentName).equalsIgnoreCase(continentName)) {
+					JOptionPane.showMessageDialog(null, "Continent name already exists!");
+				}
+
+				else if (map.searchContinent(continentName.trim()).equalsIgnoreCase(continentName.trim())) {
+					JOptionPane.showMessageDialog(null, "Continent name already exists!");
+				}
+
+				else if ((continentName != null) && (createMapEditor.getMap().searchContinent(continentName) == "")) {
+					continentArea = new ContinentArea(map);
+					continent.setContinentName(continentName);
+					createMapEditor.getMap().addContinent(continent);
+					createMapEditor.createTree();
+					loop = false;
+				}
+			} else {
+				loop = false;
+			}
+
+		}
+
+	}
+
 	public void addCountries() {
 		JTextField inputCountry = new JTextField();
 		String lastContinent = "";
@@ -156,27 +148,22 @@ public class CreateMapContoller implements ActionListener {
 					createMapEditor.createTree();
 					createMapEditor.countriesMatrix();
 					
-					/*
-					 * for(Country country : tempContinent.getCountriesPresent()) {
-					 * country.getListOfNeighbours().add(inputCountry.getText()); }
-					 */
-					
 					ArrayList<String> listOfNeighbour = new ArrayList<String>(); 
 					
-					for(Country country:map.getAllExistingCountries()){
-						if(!country.getCountryName().trim().equalsIgnoreCase(newCountry.getCountryName())){
-							listOfNeighbour.add(country.getCountryName());
-
-						}
-						newCountry.setListOfNeighbours(listOfNeighbour);
-						}
-					listOfNeighbour = null;
 					
-					for(String c:newCountry.getListOfNeighbours())
-					{
-						System.out.println(c);
+					for (Country country : map.getAllExistingCountries()) {
+						if (!country.getCountryName().trim().equalsIgnoreCase(newCountry.getCountryName()) ){
+							country.getListOfNeighbours().add(newCountry.getCountryName());
+							listOfNeighbour.add(country.getCountryName());
+						}
 					}
-					System.out.println();
+					newCountry.setListOfNeighbours(listOfNeighbour);
+					//listOfNeighbour = null;
+					
+					for(Country country:map.getAllExistingCountries()){
+						System.out.println(country.getListOfNeighbours().toString());
+						System.out.println();
+					}
 					
 					
 					loop = false;
