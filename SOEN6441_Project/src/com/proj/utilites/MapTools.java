437ce0e2e89222d9905cd0db3103360331373cd1
aman1512 <aman.ga151294@gmail.com>
2019-02-19 23:53:19 -0500

    Added functionality to check map connectivity and save ap file

--- SOEN6441_Project/src/com/proj/utilites/MapTools.java
+++ SOEN6441_Project/src/com/proj/utilites/MapTools.java
@@ -1,11 +1,14 @@
 package com.proj.utilites;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.swing.JFileChooser;
@@ -16,8 +19,16 @@ import com.proj.models.Continent;
 import com.proj.models.Country;
 import com.proj.models.Map;
 
+
 public class MapTools {
 	
+	
+	/*
+	MapTools(Map gameMap){
+		gameMap.setPath("C:\\Users\\Aman\\Desktop\\SOEN6441_Project\\SOEN6441_SVN");
+		gameMap.setName("World.map");
+	}
+*/
 	public void pickMapFile(Map gameMap){
 		
 		try {
@@ -84,8 +95,8 @@ public class MapTools {
 				 }
 			 }
 			 
-			 if(Data.toLowerCase().indexOf("continents")>=0 && Data.toLowerCase().indexOf("territories")>=0 && Data.toLowerCase().indexOf("author")>=0
-					 && Data.toLowerCase().indexOf("map")>=0){
+			 if(Data.toLowerCase().indexOf("[continents]")>=0 && Data.toLowerCase().indexOf("[territories]")>=0 && Data.toLowerCase().indexOf("author")>=0
+					 && Data.toLowerCase().indexOf("[map]")>=0){
 				 isMapValid=true;
 				 gameMap.setErrorOccurred(false);
 				 gameMap.setErrorMessage("No Errors");
@@ -140,8 +151,26 @@ public class MapTools {
 				 } 
 			 }
 			 isMapValid=checkDuplicateContinents(gameMap);
-			 isMapValid=checkDuplicateCountries(gameMap);
-			 isMapValid=checkDuplicateNeighbours(gameMap);
+			 if(isMapValid){
+				 isMapValid=checkDuplicateCountries(gameMap);
+				 if(isMapValid){
+					 isMapValid=checkDuplicateNeighbours(gameMap);
+					 if(isMapValid){
+						 isMapValid=checkDuplicateNeighbours(gameMap);
+					 }
+					 else{
+						 return false;
+					 }
+				 }
+				 else{
+					 return false;
+				 }
+			 }
+			 else{
+				 return false;
+			 }
+			 
+			 
 			 
 			 
 			 
@@ -160,7 +189,6 @@ public class MapTools {
 				 System.out.println();
 				 
 			}
-			 
 			 
 		}catch (IOException e) {
 		// TODO Auto-generated catch block
@@ -218,8 +246,169 @@ public class MapTools {
 		}
 		
 		return true;
-		
 	}
+	
+	
+	// This class represents a directed graph using adjacency 
+				// list representation 
+	class Graph 
+				{ 
+				    private int V;   // No. of vertices 
+				    private ArrayList<Integer> adj[]; //Adjacency List 
+				  
+				    //Constructor 
+				    Graph(int v) 
+				    { 
+				        V = v; 
+				        adj = new ArrayList[v]; 
+				        for (int i=0; i<v; ++i) 
+				            adj[i] = new ArrayList(); 
+				    } 
+				  
+				    //Function to add an edge into the graph 
+				    void addEdge(int v,int w) {  adj[v].add(w); }
+				  
+				    // A recursive function to print DFS starting from v 
+				    void DFSUtil(int v,Boolean visited[]) 
+				    { 
+				        // Mark the current node as visited and print it 
+				        visited[v] = true; 
+				  
+				        int n; 
+				  
+				        // Recur for all the vertices adjacent to this vertex 
+				        Iterator<Integer> i = adj[v].iterator(); 
+				        while (i.hasNext()) 
+				        { 
+				            n = i.next(); 
+				            if (!visited[n]) 
+				                DFSUtil(n,visited); 
+				        } 
+				    } 
+				  
+				    // Function that returns transpose of this graph 
+				    Graph getTranspose() 
+				    { 
+				        Graph g = new Graph(V); 
+				        for (int v = 0; v < V; v++) 
+				        { 
+				            // Recur for all the vertices adjacent to this vertex 
+				            Iterator<Integer> i = adj[v].listIterator(); 
+				            while (i.hasNext()) 
+				                g.adj[i.next()].add(v); 
+				        } 
+				        return g; 
+				    } 
+				  
+				    // The main function that returns true if graph is strongly 
+				    // connected 
+				    Boolean isSC() 
+				    { 
+				        // Step 1: Mark all the vertices as not visited 
+				        // (For first DFS) 
+				        Boolean visited[] = new Boolean[V]; 
+				        for (int i = 0; i < V; i++) 
+				            visited[i] = false; 
+				  
+				        // Step 2: Do DFS traversal starting from first vertex. 
+				        DFSUtil(0, visited); 
+				  
+				        // If DFS traversal doesn't visit all vertices, then 
+				        // return false. 
+				        for (int i = 0; i < V; i++) 
+				            if (visited[i] == false) 
+				                return false; 
+				  
+				        // Step 3: Create a reversed graph 
+				        Graph gr = getTranspose(); 
+				  
+				        // Step 4: Mark all the vertices as not visited (For 
+				        // second DFS) 
+				        for (int i = 0; i < V; i++) 
+				            visited[i] = false; 
+				  
+				        // Step 5: Do DFS for reversed graph starting from 
+				        // first vertex. Staring Vertex must be same starting 
+				        // point of first DFS 
+				        gr.DFSUtil(0, visited); 
+				  
+				        // If all vertices are not visited in second DFS, then 
+				        // return false 
+				        for (int i = 0; i < V; i++) 
+				            if (visited[i] == false) 
+				                return false; 
+				  
+				        return true; 
+				    } 
+				}
+				
+				private boolean checkMapConnectivity(Map gameMap) {
+					// TODO Auto-generated method stub			   
+					        // Create graphs given in the above diagrams 
+					    	int noOfVertices=gameMap.listOfCountryNames().size();
+					        Graph g1 = new Graph(noOfVertices); 
+					        for(int i=0;i< noOfVertices;i++){
+					        	for(Continent c:gameMap.getContinents()){
+					        		for(Country country:c.getCountriesPresent()){
+					        			if(country.getCountryName().equals(gameMap.listOfCountryNames().get(i))){
+					        				List<String> neighbours=country.getListOfNeighbours();
+					        				for(String current:neighbours){
+					        					int index=gameMap.listOfCountryNames().indexOf(current);
+					        					g1.addEdge(i, index);
+					        				}
+					        				
+					        			}
+					        		}
+					        	}
+					        }
+					      
+					        if (g1.isSC()){
+					            System.out.println("Yes, Map is strongly connected"); 
+					            return true;
+					        }
+					        else{
+					        	 System.out.println("No, Map is not strongly connected"); 
+					        	 gameMap.setErrorMessage("Map is not strongly connected");
+					        	 return false;
+					        }	        
+				}
+				
+				
+
+
+				private void saveDataIntoFile(Map gameMap,String name,String path) {
+					// TODO Auto-generated method stub
+					String data="[Map]\nauthor=Anonymous\n[Continents]\n";
+					for(Continent c:gameMap.getContinents()){
+						data=data+c.getContinentName();
+						data=data+"="+c.getControlValue();
+						data+="\n";
+					}
+					data+="[Territories]\n";
+					for(Continent c:gameMap.getContinents()){
+						for(Country country:c.getCountriesPresent()){
+							data+=country.getCountryName()+","+country.getLatitude()+","+country.getLongitude()+","+c.getContinentName()+","+String.join(",",country.getListOfNeighbours())+"\n";
+						}
+					}
+							    BufferedWriter writer;
+								try {
+									writer = new BufferedWriter(new FileWriter("E:\\NewMap.map"));
+									writer.write(data);
+									writer.close();
+								}catch(IOException e){
+									System.out.println(e.getMessage());
+								}
+				}
+				/*
+				public static void main(String args[]){
+					Map gameMap=new Map();
+					MapTools M=new MapTools(gameMap);
+					M.parseAndValidateMap(gameMap);
+					M.saveDataIntoFile(gameMap, "", "");
+					
+				}
+				*/
+				
 }
 
 
