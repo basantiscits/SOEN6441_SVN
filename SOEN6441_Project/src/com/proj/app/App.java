2b17204aa69b6c34f4d55665183d34c47101eb76
kirtidhir <kirti94.dhir@gmail.com>
2019-03-01 17:26:00 -0500

    Adding comments for main menu screen

--- SOEN6441_Project/src/com/proj/app/App.java
+++ SOEN6441_Project/src/com/proj/app/App.java
@@ -1,7 +1,12 @@
 package com.proj.app;
 import com.proj.views.*;
-
+/**
+ * Main Menu Screen Class
+ * @author Basant
+ * 
+ */
 public class App {
+	/** Creates main menu screen object. */
 	public static void main(String[] args) {
 		MainMenuScreen v= new MainMenuScreen();
 	
