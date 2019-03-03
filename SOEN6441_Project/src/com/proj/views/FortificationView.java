9dd65605105ebec0493c7627f704011611380a0c
kirtidhir <kirti94.dhir@gmail.com>
2019-03-02 21:20:12 -0500

    Added comments to Fortification View

--- SOEN6441_Project/src/com/proj/views/FortificationView.java
+++ SOEN6441_Project/src/com/proj/views/FortificationView.java
@@ -15,6 +15,12 @@ import com.proj.models.Map;
 import com.proj.models.Player;
 import com.proj.utilites.Constants;
 
+/**
+ * Fortification View class
+ * @author Kirti
+ * @since 1 Feb 2019
+ * @version 1.0
+ */
 public class FortificationView extends JFrame implements ActionListener{
 	
 	private JLabel source;
@@ -25,116 +31,226 @@ public class FortificationView extends JFrame implements ActionListener{
 	
 	private JLabel playerName;
 	
+	/**
+	 * getter for source country
+	 * @return source country
+	 */
 	public JLabel getSource() {
 		return source;
 	}
-
+	
+	/**
+	 * setter for source country
+	 * @param source name of source country
+	 */
 	public void setSource(JLabel source) {
 		this.source = source;
 	}
-
+	
+	/**
+	 * getter for destination country
+	 * @return destination country
+	 */
 	public JLabel getDestination() {
 		return destination;
 	}
-
+	
+	/**
+	 * setter for destination country
+	 * @param destination destination country
+	 */
 	public void setDestination(JLabel destination) {
 		this.destination = destination;
 	}
-
+	
+	/**
+	 * getter for number of armies
+	 * @return number of armies
+	 */
 	public JLabel getNoOfArmies() {
 		return noOfArmies;
 	}
-
+	
+	/**
+	 * setter for number of armies
+	 * @param noOfArmies number of armies
+	 */
 	public void setNoOfArmies(JLabel noOfArmies) {
 		this.noOfArmies = noOfArmies;
 	}
-
+	
+	/**
+	 * getter for player name
+	 * @return name of player
+	 */
 	public JLabel getPlayerName() {
 		return playerName;
 	}
-
+	
+	/**
+	 * setter for player name
+	 * @param playerName name of player
+	 */
 	public void setPlayerName(JLabel playerName) {
 		this.playerName = playerName;
 	}
-
+	
+	/**
+	 * getter for source country
+	 * @return source country
+	 */
 	public JComboBox getSourceCountry() {
 		return sourceCountry;
 	}
-
+	
+	/**
+	 * setter for source country
+	 * @param sourceCountry name of source country
+	 */
 	public void setSourceCountry(JComboBox sourceCountry) {
 		this.sourceCountry = sourceCountry;
 	}
-
+	
+	/**
+	 * getter for destination country
+	 * @return name of destination country
+	 */
 	public JComboBox getDestinationCountry() {
 		return destinationCountry;
 	}
-
+	
+	/**
+	 * setter for destination country
+	 * @param destinationCountry name of destination country
+	 */
 	public void setDestinationCountry(JComboBox destinationCountry) {
 		this.destinationCountry = destinationCountry;
 	}
-
+	
+	/**
+	 * getter for number of armies
+	 * @return number of armies selected
+	 */
 	public JComboBox getSelectNoOfArmies() {
 		return selectNoOfArmies;
 	}
-
+	
+	/**
+	 * setter for number of armies
+	 * @param selectNoOfArmies number of armies selected
+	 */
 	public void setSelectNoOfArmies(JComboBox selectNoOfArmies) {
 		this.selectNoOfArmies = selectNoOfArmies;
 	}
-
+	
+	/**
+	 * getter for send
+	 * @return send
+	 */
 	public JButton getSend() {
 		return send;
 	}
-
+	
+	/**
+	 * setter for send
+	 * @param send name of button
+	 */
 	public void setSend(JButton send) {
 		this.send = send;
 	}
-
+	
+	/**
+	 * getter for finish
+	 * @return finish of type JButton
+	 */
 	public JButton getFinish() {
 		return finish;
 	}
-
+	
+	/**
+	 * setter for finish
+	 * @param finish object of type JButton
+	 */
 	public void setFinish(JButton finish) {
 		this.finish = finish;
 	}
-
+	
+	/**
+	 * getter for map
+	 * @return map
+	 */
 	public Map getMap() {
 		return map;
 	}
-
+	
+	/**
+	 * setter for map
+	 * @param map object of type Map
+	 */
 	public void setMap(Map map) {
 		this.map = map;
 	}
 
-
-
+	/**
+	 * getter for Player
+	 * @return player
+	 */
 	public Player[] getPlayer() {
 		return player;
 	}
-
+	
+	/**
+	 * setter for Player
+	 * @param player array object of type Player[]
+	 */
 	public void setPlayer(Player[] player) {
 		this.player = player;
 	}
-
+	
+	/**
+	 * getter for current player
+	 * @return current player number
+	 */
 	public int getCurrentPlayer() {
 		return currentPlayer;
 	}
-
+	
+	/**
+	 * setter for current player
+	 * @param currentPlayer current player number
+	 */
 	public void setCurrentPlayer(int currentPlayer) {
 		this.currentPlayer = currentPlayer;
 	}
-
+	
+	/**
+	 * getter for Game Window
+	 * @return game window
+	 */
 	public GameWindowScreen getGameWindow() {
 		return gameWindow;
 	}
 
+	/**
+	 * setter for Game Window
+	 * @param gameWindow object of type GameWindowScreen
+	 */
 	public void setGameWindow(GameWindowScreen gameWindow) {
 		this.gameWindow = gameWindow;
 	}
-
+	
+	/**
+	 * getter for Fortification Controller
+	 * @return Fortification Controller
+	 */
 	public FortificationController getFortificationController() {
 		return fortificationController;
 	}
-
+	
+	/**
+	 * setter for Fortification Controller
+	 * @param fortificationController object of type FortificationController
+	 */
 	public void setFortificationController(FortificationController fortificationController) {
 		this.fortificationController = fortificationController;
 	}
@@ -233,6 +349,10 @@ public class FortificationView extends JFrame implements ActionListener{
 
 	}
 	
+	/**
+	 * Adds country to combo box
+	 * @param country name of combo box
+	 */
 	public void addCountryToBox(JComboBox country) {
 		country.removeAllItems();
 		for(Country c : player[currentPlayer].getCountriesOwned()) {
@@ -240,6 +360,10 @@ public class FortificationView extends JFrame implements ActionListener{
 		}
 	}
 	
+	/**
+	 * Adds destination countries to list of destination countries
+	 * @param sourCountry name of source country
+	 */
 	public void addDestCountries(Country sourCountry) {
 		destinationCountry.removeAllItems();
 		for(Country c : player[currentPlayer].getCountriesOwned()) {
@@ -249,6 +373,9 @@ public class FortificationView extends JFrame implements ActionListener{
 		}
 	}
 	
+	/**
+	 * Adds armies to selected number of armies
+	 */
 	public void AddArmies() {
 		selectNoOfArmies.removeAllItems();
 		for(int i=1;i < Integer.valueOf(armiesInSource.getText());i++) {
@@ -256,22 +383,41 @@ public class FortificationView extends JFrame implements ActionListener{
 		}
 	}
 	
+	/**
+	 * getter for armies in source country
+	 * @return number of armies in source country
+	 */
 	public JLabel getArmiesInSource() {
 		return armiesInSource;
 	}
-
+	
+	/**
+	 * setter for armies in source country
+	 * @param armiesInSource number of armies in source country
+	 */
 	public void setArmiesInSource(JLabel armiesInSource) {
 		this.armiesInSource = armiesInSource;
 	}
 
+	/**
+	 * getter for armies in destination country
+	 * @return number of armies in destination country
+	 */
 	public JLabel getArmiesInDestination() {
 		return armiesInDestination;
 	}
-
+	
+	/**
+	 * setter for armies in destination country
+	 * @param armiesInDestination number of armies in destination country
+	 */
 	public void setArmiesInDestination(JLabel armiesInDestination) {
 		this.armiesInDestination = armiesInDestination;
 	}
-
+	
+	/**
+	 * action performed method
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
