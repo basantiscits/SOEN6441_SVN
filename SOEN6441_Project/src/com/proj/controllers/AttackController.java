5fc6adbb08893ee6c58e651277443615ba84f60e
aman1512 <aman.ga151294@gmail.com>
2019-03-18 16:27:01 -0400

    attach phase controller template created

--- SOEN6441_Project/src/com/proj/controllers/AttackController.java
+++ SOEN6441_Project/src/com/proj/controllers/AttackController.java
@@ -1,7 +1,8 @@
 package com.proj.controllers;
 
 import java.awt.event.ActionEvent;
-
+import java.util.ArrayList;
+import java.util.Random;
 
 import com.proj.views.AttackView;
 
@@ -9,11 +10,33 @@ import com.proj.views.AttackView;
 public class AttackController {
 	//private Country sourCountry, destCountry;
 	private AttackView AttackView;
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public String attacker;
+	public String defender;
+	public Random diceRoll;
+	public ArrayList<Integer> attackerDiceValues;
+	public ArrayList<Integer> defenderDiceValues;
+	
+	
+	AttackController(){
+		diceRoll=new Random();
 	}
-
-
+	
+	public boolean normalAttack(String attacker,String defender,int noOfArmiesSelected){
+		if(noOfArmiesSelected==2){
+			
+		}
+		else if(noOfArmiesSelected==3){
+			
+		}
+		else{
+			
+		}
+		return true;
+	}
+	
+	public boolean allOutAttack(String attacker, String defender){
+		
+		return false;
+		
+	}
 }
