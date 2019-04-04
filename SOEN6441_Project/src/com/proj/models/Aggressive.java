9601bc2157ef7ce724782d0e23aa158332c57ed2
ofreish <36609103+ofreish@users.noreply.github.com>
2019-04-04 23:25:09 +0530

    Aggressive player implemented without attack phase

--- SOEN6441_Project/src/com/proj/models/Aggressive.java
+++ SOEN6441_Project/src/com/proj/models/Aggressive.java
@@ -1,22 +1,49 @@
 package com.proj.models;
 
+
 public class Aggressive implements BehaviorStrategies {
 
 	@Override
 	public void startUpPhase(GameModelCreation gameModel) {
 		// TODO Auto-generated method stub
+		Country country = gameModel.getCurrPlayer().getCountriesOwned().get(0);
+		if ( gameModel.getCurrPlayer().getNoOfArmiesOwned() > 0) {
+			country.addNoOfArmiesCountry();
+			gameModel.getCurrPlayer().reduceArmyInPlayer();
+		}
+		
+	}
+	
+	private Country maxArmiesInCountry(Player player) {
+		Country country = player.getCountriesOwned().get(0);
+		int noOfArmies = country.getNoOfArmiesPresent();
+		for(Country c : player.getCountriesOwned()) {
+			if(c.getNoOfArmiesPresent() > noOfArmies) {
+				country = c;
+				noOfArmies = c.getNoOfArmiesPresent();
+			}
+		}
+		return country;
 		
 	}
 
 	@Override
 	public void reinforcementPhase(GameModelCreation gameModel) {
 		// TODO Auto-generated method stub
+		Country country = maxArmiesInCountry(gameModel.getCurrPlayer());
+		while(gameModel.getCurrPlayer().getNoOfArmiesOwned()>0) {
+			country.addNoOfArmiesCountry();
+			gameModel.getCurrPlayer().reduceArmyInPlayer();
+		}
+		gameModel.setGameState(2);
+		attackPhase(gameModel);
 		
 	}
 
 	@Override
 	public void attackPhase(GameModelCreation gameModel) {
 		// TODO Auto-generated method stub
+		Country country = maxArmiesInCountry(gameModel.getCurrPlayer());
 		
 	}
 
@@ -24,6 +51,22 @@ public class Aggressive implements BehaviorStrategies {
 	public void fortificationPhase(GameModelCreation gameModel) {
 		// TODO Auto-generated method stub
 		
+		Country mainCountry = maxArmiesInCountry(gameModel.getCurrPlayer());
+		//List<Country> neighbors = mainCountry.
+		
+		
+		for(Country c : gameModel.getCurrPlayer().getCountriesOwned()) {
+			if (mainCountry.getListOfNeighbours().contains(c.getCountryName())) {
+				while(c.getNoOfArmiesPresent()>1) {
+					mainCountry.addNoOfArmiesCountry();
+					c.removeNoOfArmiesCountry();
+				}
+			}
+		}
+		
+		gameModel.incrementTurn();
+		gameModel.changePlayer();
+		gameModel.setGameState(1);
 	}
 
 }
